7828b904f328c1844c2314b9c228700245a0b273
Christopher Dosin <hello@christopherdosin.me>
2019-06-10 16:54:49 +0300

    Added detail entry api endpoint

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -25,6 +26,33 @@ class BlogController extends AbstractController
         $blogRepository = $this->container->get('sas_blog_entries.repository');
 
         $criteria = new Criteria();
+
+        $criteria->addFilter(
+            new EqualsFilter('active', true)
+        );
+
+        $results = $blogRepository->search($criteria, $context);
+
+        return new JsonResponse($results);
+    }
+
+    /**
+     * @Route("/sales-channel-api/v1/sas/get-blog-entry/{slug}", name="sales-channel-api.action.sas.get-blog-entry", methods={"GET"})
+     * @param Context $context
+     * @param $slug
+     * @return JsonResponse
+     * @throws InconsistentCriteriaIdsException
+     */
+    public function getBlogEntry(Context $context, $slug): JsonResponse
+    {
+        /** @var EntityRepositoryInterface $blogRepository */
+        $blogRepository = $this->container->get('sas_blog_entries.repository');
+
+        $criteria = new Criteria();
+
+        $criteria->addFilter(
+            new EqualsFilter('slug', $slug)
+        );
 
         $results = $blogRepository->search($criteria, $context);
 
