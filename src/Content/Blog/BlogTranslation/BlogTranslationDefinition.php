55bfd780c58313574a5a8ac43bfec80b0fa3761d
Christopher Dosin <hello@christopherdosin.me>
2020-10-30 14:18:11 +0200

    Added required fields to definition

--- src/Content/Blog/BlogTranslation/BlogTranslationDefinition.php
+++ src/Content/Blog/BlogTranslation/BlogTranslationDefinition.php
@@ -3,7 +3,6 @@ namespace Sas\BlogModule\Content\Blog\BlogTranslation;
 
 use Sas\BlogModule\Content\Blog\BlogEntriesDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityTranslationDefinition;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\JsonField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
@@ -33,12 +32,12 @@ class BlogTranslationDefinition extends EntityTranslationDefinition
     protected function defineFields(): FieldCollection
     {
         return new FieldCollection([
-            (new StringField('title', 'title'))->addFlags(new Required()),
-            (new StringField('slug', 'slug'))->addFlags(new Required()),
+            new StringField('title', 'title'),
+            new StringField('slug', 'slug'),
             new StringField('teaser', 'teaser'),
             new StringField('meta_title', 'metaTitle'),
             new StringField('meta_description', 'metaDescription'),
-            (new JsonField('content', 'content'))->addFlags(new Required()),
+            new JsonField('content', 'content'),
         ]);
     }
 }
